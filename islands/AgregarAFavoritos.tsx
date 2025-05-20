5cc7bdf16783b6ee2ba7fe41a4134e463b6e8372
Álvaro Pereda Sánchez <121577598+AlvaroPereda@users.noreply.github.com>
2025-05-20 15:24:36 +0000

    no more repeated films

--- islands/AgregarAFavoritos.tsx
+++ islands/AgregarAFavoritos.tsx
@@ -5,11 +5,20 @@ import { Favoritos } from "../utils/favoritos.ts";
 const AgregarAFavoritos: FunctionalComponent = () => {
 
     const [pelicula, setPelicula] = useState<string>("")
+    const [repetida, setRepetida] = useState<boolean>(false)
+    
+    const comprobarPelicula = () => {
+
+        const peliRepetida = Favoritos.value.some(e => e === pelicula)
+        setRepetida(peliRepetida)
+        if(!peliRepetida) Favoritos.value = [...Favoritos.value, pelicula]
+    }
 
     return (
         <div>
             <input type="text" onBlur={(e) => setPelicula(e.currentTarget.value)}/>
-            <button type="button" onClick={() => Favoritos.value = [...Favoritos.value, pelicula]}>Agregar a favoritos</button>
+            <button type="button" onClick={() => comprobarPelicula()}>Agregar a favoritos</button>
+            {repetida && <h3>Ya has agregado esa película</h3>}
         </div>
     )
 }
